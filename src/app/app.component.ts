5d4d576a77844521beb2fbd7d9596f1c335535eb
Jake Smith <jakehockey10@gmail.com>
2017-04-04 21:48:40 -0600

    move heroes from AppComponent to HeroesComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from 'app/hero';
-import { HeroService } from 'app/hero.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HeroService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    // this.heroeService.getHeroesSlowly()
-    this.heroService.getHeroes()
-      .subscribe(
-      (heroes) => this.heroes = heroes,
-      (error) => console.log(error)
-      );
-  }
+  constructor() { }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from 'app/hero';
+import { HeroService } from 'app/hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    // this.heroeService.getHeroesSlowly()
+    this.heroService.getHeroes()
+      .subscribe(
+      (heroes) => this.heroes = heroes,
+      (error) => console.log(error)
+      );
   }
 
 }
